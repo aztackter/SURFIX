7b199289767d12a75458d2cfc2b37cc6bd86950b
aztackter <aztackter@gmail.com>
2026-03-25 21:01:20 -0400

    Create index.js

---
+++ api/providers/index.js
@@ -0,0 +1,19 @@
+import VidSrc from './vidSrc.js';
+import VixSrc from './vixSrc.js';
+import VidRock from './vidRock.js';
+import Uembed from './uembed.js';
+import RgShows from './rgShows.js';
+import VidZee from './vidZee.js';
+import Embed from './embed.js';
+import MovieDownloader from './movieDownloader.js';
+
+export default [
+  VidSrc,
+  VixSrc,
+  VidRock,
+  Uembed,
+  RgShows,
+  VidZee,
+  Embed,
+  MovieDownloader
+];
